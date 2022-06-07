8dd0376b12e69d19e92deed0bd537179ed24ee29
amclain21 <36746650+amclain21@users.noreply.github.com>
2022-06-07 17:29:37 -0500

    Added call to index for input.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const host = 'localhost';
 const port = 7000;
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
+// false = input on Digital Signage; true = input on CCTV/Airtame //
+let currentInput = false;
+
 const requestListener = function(req, res) {
 	let data = '';
 	req.on('data', chunk => {
@@ -96,6 +99,28 @@ const requestListener = function(req, res) {
 		} else if (data == 'mute') {
 			console.log('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO KEY_MUTE');
 			exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO KEY_MUTE');
+			res.setHeader("Content-Type", "application/json");
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'post');
+			res.setHeader('Access-Control-Max-Age', 5000);
+			res.writeHead(200);
+			res.end(`{"message": "success"}`);
+		} else if (data == 'input') {
+			console.log('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+			if (currentInput) {
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+				sleep(200);
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+			} else {
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+				sleep(200);
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+				sleep(200);
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+				sleep(200);
+				exec('irsend SEND_ONCE --device=/var/run/lirc/lircd VIZIO INPUT');
+			}
+
 			res.setHeader("Content-Type", "application/json");
 			res.setHeader('Access-Control-Allow-Origin', '*');
 			res.setHeader('Access-Control-Allow-Methods', 'post');
